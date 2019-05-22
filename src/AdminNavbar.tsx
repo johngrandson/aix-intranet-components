d2ef17dca30127a31219c3e36546251c6382e7e0
Henrique Nunes <henrique_nunes@hotmail.com.br>
2019-05-22 16:48:26 -0300

    navbar agora exibe usuário do token

--- src/AdminNavbar.tsx
+++ src/AdminNavbar.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Component } from "react";
-
+import Avatar from "react-avatar";
+import * as jwt from 'jsonwebtoken';
 // reactstrap components
 import {
   DropdownMenu,
@@ -10,20 +11,99 @@ import {
   Navbar,
   Nav,
   Container,
-  Media
+  Media,
+  Spinner
 } from "reactstrap";
 
-interface OwnProps {
+interface User {
+  name: string,
+  username: string,
+  pictureUrl: string
+}
+
+interface Props {
   brandText: string;
 }
 
-type Props = OwnProps;
+interface State {
+  isLoadingUserData: boolean,
+  user?: User
+}
 
-class AdminNavbar extends Component<Props> {
-  state = {
-    user: {
-      name: 'João Netto'
+class AdminNavbar extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isLoadingUserData: true
+    };
+  }
+
+  public async componentDidMount() {
+    const token: string | null = localStorage.getItem("token");
+
+    if (token) {
+      try {
+        const decodedToken = jwt.decode(token);
+
+        if (decodedToken) {
+          this.setState({
+            user: decodedToken['user'] as User
+          });
+        }
+      } catch {}
     }
+
+    this.setState({
+      isLoadingUserData: false
+    });
+  }
+
+  private logout() {
+    localStorage.removeItem("token");
+    window.location.href = '/';
+  }
+
+  private renderUserMenu() {
+    const { isLoadingUserData, user } = this.state;
+
+    if (isLoadingUserData) {
+      return <Spinner color="light" size="sm" />;
+    }
+
+    if (user) {
+      return (
+        <UncontrolledDropdown nav>
+          <DropdownToggle className="pr-0" nav>
+            <Media className="align-items-center">
+              <Avatar round={true} name={user.name} size="36" />
+              <Media className="ml-2 d-none d-lg-block">
+                <span className="mb-0 text-sm font-weight-bold">
+                  {user.name}
+                </span>
+              </Media>
+            </Media>
+          </DropdownToggle>
+          <DropdownMenu className="dropdown-menu-arrow" right>
+
+            <DropdownItem to="/admin/user-profile" disabled>
+              <i className="ni ni-single-02" />
+              <span>Meu perfil</span>
+            </DropdownItem>
+            <DropdownItem to="/admin/user-profile" disabled>
+              <i className="ni ni-settings-gear-65" />
+              <span>Ajustes</span>
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem onClick={this.logout}>
+              <i className="ni ni-user-run" />
+              <span>Sair</span>
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      );
+    }
+
+    return <></>
   }
 
   render() {
@@ -31,43 +111,9 @@ class AdminNavbar extends Component<Props> {
       <>
         <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
           <Container fluid>
-            <h1 className="text-white">{this.props.brandText}</h1>
+            <h1 className="text-white" style={{fontWeight:300}}>{this.props.brandText}</h1>
             <Nav className="align-items-center d-none d-md-flex" navbar>
-              <UncontrolledDropdown nav>
-                <DropdownToggle className="pr-0" nav>
-                  <Media className="align-items-center">
-                    <span className="avatar avatar-sm rounded-circle">
-                      <img
-                        alt="..."
-                        src="/bg.jpg"
-                      />
-                    </span>
-                    <Media className="ml-2 d-none d-lg-block">
-                      <span className="mb-0 text-sm font-weight-bold">
-                        {this.state.user.name}
-                      </span>
-                    </Media>
-                  </Media>
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-arrow" right>
-                  <DropdownItem className="noti-title" header tag="div">
-                    <h6 className="text-overflow m-0">Welcome!</h6>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile">
-                    <i className="ni ni-single-02" />
-                    <span>Meu perfil</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile">
-                    <i className="ni ni-settings-gear-65" />
-                    <span>Ajustes</span>
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem href="#pablo" onClick={e => e.preventDefault()}>
-                    <i className="ni ni-user-run" />
-                    <span>Logout</span>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+              {this.renderUserMenu()}
             </Nav>
           </Container>
         </Navbar>
